3f2a44f8e81e4a48f4070a86fdd81d939de68332
adityaseptemberiano <ponsipon249@gmail.com>
2025-07-03 11:01:14 +0700

    finalprojedsfdsf

--- config/db.php
+++ config/db.php
@@ -1,9 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "keuangan_db";
-
+$host = "db";              
+$user = "root";           
+$pass = "root";           
+$dbname = "keuangan_db";  
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
