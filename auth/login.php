92dd2e8268e652eeb4a0f85ebe8d91f46f98f14c
adityaseptemberiano <ponsipon249@gmail.com>
2025-07-03 12:41:04 +0700

    yallahplis

--- auth/login.php
+++ auth/login.php
@@ -2,7 +2,7 @@
 session_start();
 include '../config/db.php';
 
-// Tampilkan error (bisa di-nonaktifkan di production)
+/
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
--- config/db.php
+++ config/db.php
@@ -3,6 +3,10 @@ $host = getenv("MYSQLHOST");
 $user = getenv("MYSQLUSER");
 $pass = getenv("MYSQLPASSWORD");
 $dbname = getenv("MYSQL_DATABASE"); 
+echo "MYSQLHOST: " . getenv("MYSQLHOST") . "<br>";
+echo "MYSQLUSER: " . getenv("MYSQLUSER") . "<br>";
+echo "MYSQLPASSWORD: " . getenv("MYSQLPASSWORD") . "<br>";
+echo "MYSQL_DATABASE: " . getenv("MYSQL_DATABASE") . "<br>";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
