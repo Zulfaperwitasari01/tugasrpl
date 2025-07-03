59bd8cd7c383c59b45146db54733c856c40f22b2
adityaseptemberiano <ponsipon249@gmail.com>
2025-07-03 11:19:26 +0700

    finalprojebissmilahselesai

--- config/db.php
+++ config/db.php
@@ -1,8 +1,9 @@
 <?php
-$host = "tugasrpl";              
-$user = "root";           
-$pass = "root";           
-$dbname = "keuangan_db";  
+$host = getenv("MYSQLHOST") ?: "localhost";
+$user = getenv("MYSQLUSER") ?: "root";
+$pass = getenv("MYSQLPASSWORD") ?: "";
+$dbname = getenv("MYSQLDATABASE") ?: "keuangan_db";
+
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
